a7d7ba46b18aed82142b5439bb84bc1cd7543dd8
Mateus Koehler <mateus@mateuskoehler.com>
2019-04-07 18:01:00 -0400

    First ommit

---
+++ src/components/Splash.js
@@ -0,0 +1,9 @@
+import React from "react"
+import keyboardImage from "../images/dactyl-manuform-2.jpg"
+import "../styles/splash.css"
+
+const Splash = () => {
+  return <img className={"splashContainer"} src={keyboardImage} />
+}
+
+export default Splash
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import Splash from "../components/Splash"
+import "../styles/index.css"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div className="gridContainer">
+    <Splash />
+
+    <div style={{ gridArea: "bio", backgroundColor: "blue" }}>Bio</div>
+    <div
+      style={{ gridArea: "projects", backgroundColor: "teal", height: "100%" }}
+    />
+  </div>
+)
